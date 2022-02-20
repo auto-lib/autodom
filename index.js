d500b7e391e93174a23826c4d43b822e5350cdb4
Karl Penzhorn <karlpenzhorn@gmail.com>
2022-02-20 11:10:19 +0200

    kindof working

--- index.js
+++ index.js
@@ -75,26 +75,100 @@ function is_jsx(node) {
 
 console.log('hello');
 
+function add_obj(obj, ast, base) {
+
+    base = base || '';
+
+    Object.keys(ast).forEach(name => {
+
+        if (name == 'props') {
+            Object.keys(ast[name]).forEach(prop => {
+                obj[base + prop] = ast.props[prop];
+            });
+        } else if (name == 'children') {
+
+        } else
+            obj[base + name] = ast[name];
+
+    })
+
+}
+
 export function render(obj, root) {
-    console.log('render');
-    return;
+
     obj['root'] = root;
     obj['mount'] = _ => _.root.append(_.el);
+
+    console.log('render obj', obj);
+
     auto(obj);
 };
 
-export function jsx(tag, props, children) {
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
-    console.log('tags');
+export function jsx(tag, props) {
+
+    if (typeof tag !== 'string') {
+        let obj = new tag();
+        obj.props = props;
+        let bound = new tag().render.bind(obj);
+        let ret = bound();
+        return ret;
+    }
 
     let obj = {};
 
-    obj['el'] = document.createElement(tag);
 
-    props.forEach(name => obj[name] = props[name]);
+    // obj['tag'] = tag;
+    // obj['props'] = {};
 
-    console.log('obj', obj);
+    // Object.keys(props).forEach(name => {
+    //     if (name != 'children') obj.props[name] = props[name];
+    //     else
+    //         obj['children'] = props[name];
+    // });
+
+    // let ref = randomIntFromInterval(0, 1000).toLocaleString('en-US', { minimumIntegerDigits: 3, useGrouping: false });
+
+    obj['tag'] = tag;
+    obj['el'] = (_) => document.createElement(_.tag)
+    obj['mount'] = () => {};
+
+    Object.keys(props).forEach(name => {
+        if (name != 'children') obj[name] = props[name];
+        else {
+            let children = props[name];
+            if (Array.isArray(children)) {
+                let i = 0;
+                children.forEach(child => {
+                    Object.keys(child).forEach(name => {
+                        let ref = 'child' + i + '_' + name;
+                        if (ref.indexOf('_el') > -1)
+                            obj[ref] = new Function("_", "return document.createElement(_." + ref.replace('_el', '_tag') + ")");
+                        else if (ref.indexOf('_mount') > -1)
+                            obj[ref] = new Function("_", "_." + name.replace('mount', 'el') + ".append(_." + ref.replace('mount', 'el') + ')');
+                        //     obj[ref] = new Function("_", fn);
+                        //     console.log('ref', ref);
+                        //     fn = '_.el.append(_.' + ref + ')';
+
+                        //     //fn = '_.' + ref + '.append(_.' + ref.replace('child' + i + '_', '') + ')';
+                        //     console.log('fn', fn);
+                        //     obj[ref.replace('_el', '_mount')] = new Function('_', fn);
+                        // } else
+                        else obj[ref] = child[name];
+                    })
+
+                    i += 1;
+                })
+            }
+        }
+    });
+    // console.log('obj', obj);
 
     return obj;
 
 };
+
+export function jsxs(tag, props) { return jsx(tag, props); }
