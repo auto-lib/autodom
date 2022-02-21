bf545881590538299bdfc31c93d7a0fe8f418b8c
Karl Penzhorn <karlpenzhorn@gmail.com>
2022-02-21 11:07:10 +0200

    react tutorial equivalence working

--- index.js
+++ index.js
@@ -94,7 +94,51 @@ function add_obj(obj, ast, base) {
 
 }
 
-export function render(obj, root) {
+function add_child(obj, child, base, i) {
+    obj = Object.assign(obj, get_obj(child, base + 'child' + i + '_'));
+    obj[base + 'mount' + i] = new Function('_', '_.' + base + 'el.append(_.' + base + 'child' + i + '_el)');
+    return obj;
+}
+
+function get_obj(ast, base) {
+
+    base = base || '';
+
+    let obj = {}
+
+    obj[base + 'tag'] = ast.tag;
+    obj[base + 'el'] = new Function('_', 'return document.createElement(_.' + base + 'tag)');
+
+    if (ast.props) Object.keys(ast.props).forEach((name, i) => {
+
+        let n = name == 'className' ? 'class' : name;
+        let fn = '_.' + base + 'el.setAttribute("' + n + '","' + ast.props[name] + '")';
+        obj[base + 'prop' + i] = new Function('_', fn);
+    })
+
+    if (typeof ast.children !== 'undefined') {
+        if (Array.isArray(ast.children))
+            ast.children.forEach((child, i) => {
+                obj = add_child(obj, child, base, i);
+            });
+        else {
+            if (typeof ast.children == 'object')
+                obj = add_child(obj, ast.children, base, 0);
+            else {
+                let fn = '_.' + base + 'el.innerHTML = "' + ("" + ast.children) + '"';
+                obj[base + 'text'] = new Function('_', fn);
+            }
+        }
+    }
+
+    return obj;
+}
+
+export function render(ast, root) {
+
+    console.log('ast', ast);
+
+    let obj = get_obj(ast);
 
     obj['root'] = root;
     obj['mount'] = _ => _.root.append(_.el);
@@ -121,14 +165,16 @@ export function jsx(tag, props) {
     let obj = {};
 
 
-    // obj['tag'] = tag;
-    // obj['props'] = {};
+    obj['tag'] = tag;
+    obj['props'] = {};
 
-    // Object.keys(props).forEach(name => {
-    //     if (name != 'children') obj.props[name] = props[name];
-    //     else
-    //         obj['children'] = props[name];
-    // });
+    Object.keys(props).forEach(name => {
+        if (name != 'children') obj.props[name] = props[name];
+        else
+            obj['children'] = props[name];
+    });
+
+    return obj;
 
     // let ref = randomIntFromInterval(0, 1000).toLocaleString('en-US', { minimumIntegerDigits: 3, useGrouping: false });
 
